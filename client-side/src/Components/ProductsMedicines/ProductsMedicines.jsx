75b33c380c02068a273b504b30d1fe8976574ed4
Ashiq <khanashiq4494@gmail.com>
2022-05-15 13:04:52 +0530

    ProductsMedicines Completed

---
+++ client-side/src/Components/ProductsMedicines/ProductsMedicines.jsx
@@ -0,0 +1,13 @@
+import MedicineSearch from "../MedicineSearch/MedicineSearch";
+import MedicinesProduct from "../MedicinesProduct/MedicinesProduct";
+
+import React from "react";
+
+export default function ProductsMedicines() {
+  return (
+    <div id="prodmed">
+      <MedicineSearch />
+      <MedicinesProduct />
+    </div>
+  );
+}
